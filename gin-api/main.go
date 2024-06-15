201d20547d6781bdf7eea3b88df7fe343015497d
c1101081 <c1101081@planet.kanazawa-it.ac.jp>
2024-06-15 05:12:51 +0000

    ある程度できた

--- gin-api/main.go
+++ gin-api/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -32,22 +33,51 @@ func main() {
 	})
 
 	r.GET("my-health", func(c *gin.Context) {
-		// FitbitAPIへのリクエストをする
-		c.Request.URL.Path = "https://api.fitbit.com/1/user/-/profile.json"
-		c.Request.Header.Add("Authorization", "Bearer " + os.Getenv("FITBIT_AUTH"))
-		// FitbitAPIからのレスポンスを受け取る
-		res, err := http.DefaultClient.Do(c.Request)
+		err := godotenv.Load(fmt.Sprintf("./env/%s.env", os.Getenv("GO_ENV")))
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("Error loading .env file")
+		}
+
+		fitbit_auth := os.Getenv("FITBIT_AUTH")
+		fitbit_api_url_core := "https://api.fitbit.com/1/"
+
+		client := &http.Client {}
+
+		req, err := http.NewRequest("GET", fitbit_api_url_core + "user/-/profile.json", nil)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		req.Header.Add("Authorization", "Bearer " + fitbit_auth)
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 		defer res.Body.Close()
+
 		body, err := ioutil.ReadAll(res.Body)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(body))
+		
+		// 次の課題
+		// https://maku77.github.io/p/dsbs9p5/#google_vignette
+		
+		health := []byte{}
+		
+		health,err = json.MarshalIndent(string(body), "", "    ")
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 
 		c.JSON(200,gin.H {
-			"result": string(body),
+			"result": health, // string(body),
 		})
 	})
 
